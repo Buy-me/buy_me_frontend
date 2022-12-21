49c82a25f5553fdbe952b8f7ca081027e7d955bd
PhamneyuT <koduapass@gmail.com>
2022-12-21 22:40:10 +0700

    added edit account screen

--- src/screens/Account/EditAccount.js
+++ src/screens/Account/EditAccount.js
@@ -1,9 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { COLORS, icons, SIZES } from '../../constants'
-import { Header, IconButton } from '../../component'
+import { FormInput, FormInputCheck, GrayLayout, Header, IconButton, TextButton } from '../../component'
+import { useState } from 'react'
+import Utils from "../../utils";
 
 const EditAccount = ({ navigation }) => {
+    const { utils } = Utils
+
+    const [fullName, setFullName] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const [idCard, setIdCard] = useState("")
+    //date of birth
+    //gender
+    const [email, setEmail] = useState("")
+    const [address, setAddress] = useState("")
+
+    const [errorMsg, setErrorMsg] = useState("")
+
     const renderHeader = () => {
         return (
             <Header
@@ -21,10 +35,151 @@ const EditAccount = ({ navigation }) => {
         )
     }
 
+    const renderFooter = () => {
+        return (
+            <View style={{
+                flexDirection: "row",
+                height: 100,
+                alignItems: "center",
+                paddingHorizontal: SIZES.padding,
+                paddingBottom: SIZES.radius,
+            }}>
+                <TextButton
+                    buttonStyle={{
+                        flex: 1,
+                        flexDirection: "row",
+                        height: 60,
+                        marginLeft: SIZES.radius,
+                        paddingHorizontal: SIZES.radius,
+                        borderRadius: SIZES.radius,
+                        backgroundColor: COLORS.primary
+                    }}
+                    label={"Save"}
+                    onPress={() => {
+                        navigation.goBack()
+                    }}
+                />
+            </View>
+        )
+    }
+
     return (
         <View style={styles.container}>
             {/* Header */}
             {renderHeader()}
+
+            <ScrollView>
+                <View style={styles.contentContainer}>
+                    <GrayLayout>
+                        <FormInput
+                            label={"Full Name"}
+                            maxLength={50}
+                            value={fullName}
+                            inputContainerStyle={{
+                                backgroundColor: "white",
+                            }}
+                            onChange={(value) => {
+                                setFullName(value)
+                                utils.validateInput(value, 0, setErrorMsg)
+                            }}
+                            errorMsg={errorMsg}
+                        />
+
+                        <FormInput
+                            label={"Phone Number"}
+                            maxLength={11}
+                            value={phoneNumber}
+                            keyboardType="numeric"
+                            containerStyle={{ marginTop: 15 }}
+                            inputContainerStyle={{
+                                backgroundColor: "white",
+                            }}
+                            onChange={(value) => {
+                                setPhoneNumber(value)
+                                utils.validateInput(value, 0, setErrorMsg)
+                            }}
+                            errorMsg={errorMsg}
+                        />
+
+                        <FormInput
+                            label={"ID Card"}
+                            maxLength={10} //????
+                            keyboardType="numeric"
+                            value={idCard}
+                            containerStyle={{ marginTop: 15 }}
+                            inputContainerStyle={{
+                                backgroundColor: "white",
+                            }}
+                            onChange={(value) => {
+                                setIdCard(value)
+                                utils.validateInput(value, 0, setErrorMsg)
+                            }}
+                            errorMsg={errorMsg}
+                        />
+
+                        <FormInput
+                            label={"Date of Birth"}
+                            // value={phoneNumber}
+                            placeholder="DD/MM/YYYY"
+                            containerStyle={{ marginTop: 15 }}
+                            inputContainerStyle={{
+                                backgroundColor: "white",
+                            }}
+                            onChange={(value) => {
+                                // setPhoneNumber(value)
+                                utils.validateInput(value, 0, setErrorMsg)
+                            }}
+                            errorMsg={errorMsg}
+                        />
+
+                        <FormInput
+                            label={"Gender"}
+                            // value={gender}
+                            placeholder="Select gender"
+                            containerStyle={{ marginTop: 15 }}
+                            inputContainerStyle={{
+                                backgroundColor: "white",
+                            }}
+                            onChange={(value) => {
+                                // setPhoneNumber(value)
+                                utils.validateInput(value, 0, setErrorMsg)
+                            }}
+                            errorMsg={errorMsg}
+                        />
+
+                        <FormInput
+                            label={"Email"}
+                            value={email}
+                            containerStyle={{ marginTop: 15 }}
+                            inputContainerStyle={{
+                                backgroundColor: "white",
+                            }}
+                            onChange={(value) => {
+                                setEmail(value)
+                                utils.validateInput(value, 0, setErrorMsg)
+                            }}
+                            errorMsg={errorMsg}
+                        />
+
+                        <FormInput
+                            label={"Address"}
+                            value={address}
+                            containerStyle={{ marginTop: 15 }}
+                            inputContainerStyle={{
+                                backgroundColor: "white",
+                            }}
+                            onChange={(value) => {
+                                setAddress(value)
+                                utils.validateInput(value, 0, setErrorMsg)
+                            }}
+                            errorMsg={errorMsg}
+                        />
+                    </GrayLayout>
+                </View>
+            </ScrollView>
+
+            {/* Footer */}
+            {renderFooter()}
         </View>
     )
 }
@@ -57,5 +212,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: COLORS.white
+    },
+    contentContainer: {
+        marginHorizontal: SIZES.padding,
+        paddingVertical: 15
     }
 })
