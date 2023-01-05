9befc6d16f5dcd3f3ffb890771e2cd1eda2c027c
toilathaibinh <binhdinhqt137@gmail.com>
2023-01-05 17:11:26 +0700

    fix: ipload avatar

--- src/api/profileApi.js
+++ src/api/profileApi.js
@@ -1,62 +1,52 @@
 import { Platform } from "react-native";
 import utils from "../utils";
 import privateClient from "./client";
+import publicClient from "./publicClient";
 
 const profileEndpoints = {
-    get: "/profile",
-    patch: "/profile",
-    postImage: "/upload"
+  get: "/profile",
+  patch: "/profile",
+  postImage: "/upload",
 };
 
 const profileApi = {
-    getProfile: async () => {
-        try {
-            const response = await privateClient.get(profileEndpoints.get)
-            return { response }
-        }
-        catch (err) {
-            return { err }
-        }
-    },
-    updateProfile: async (data) => {
-        try {
-            const response = await privateClient.patch(profileEndpoints.patch, {
-                ...data
-            })
-            return { response }
-        }
-        catch (err) {
-            return { err }
-        }
-    },
-    uploadAvatar: async (data) => {
-        console.log(data);
-
-        let fileUrl = Platform.OS === "ios" ? data.url.replace('file://', '') : data.url
-        let fileName = utils.utils.getFileName(fileUrl)
-        let fileType = utils.utils.getFileType(fileUrl)
-        // console.log(fileUrl);
-        // console.log(fileType);
-
-        const form = new FormData()
-        form.append("file", {
-            uri: fileUrl,
-            name: fileName,
-            type: fileType
-        })
-        // form.append("folder", "avatar")
-
-        console.log(form);
-        try {
-            const response = await privateClient.post(profileEndpoints.postImage, form, {
-                headers: {'Content-Type': 'multipart/form-data'}
-            })
-            return { response }
-        }
-        catch (err) {
-            return { err }
-        }
+  getProfile: async () => {
+    try {
+      const response = await privateClient.get(profileEndpoints.get);
+      return { response };
+    } catch (err) {
+      return { err };
     }
-}
+  },
+  updateProfile: async (data) => {
+    try {
+      const response = await privateClient.patch(profileEndpoints.patch, {
+        ...data,
+      });
+      return { response };
+    } catch (err) {
+      return { err };
+    }
+  },
+  uploadAvatar: async (avatar) => {
+    let formData = new FormData();
 
-export default profileApi
+    formData.append("file", avatar);
+    formData.append("folder", "avatar");
+
+    try {
+      const response = await publicClient.post(
+        profileEndpoints.postImage,
+        formData,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+      return { response };
+    } catch (err) {
+      return { err };
+    }
+  },
+};
+
+export default profileApi;
